96df88b9b714e38c88990b6e0f371e49a2b83a79
KyleRattet <krattet@gmail.com>
2015-08-11 14:50:24 -0600

    base games being created and put on DOM

--- js/main.js
+++ js/main.js
@@ -1,11 +1,71 @@
+var Game = function(title, type){
+ this.title = title;
+ this.type = type;
+};
 
-var Game = function (title,genre) {
-  this.title = title;
-  this.genre = genre;
+
+Game.prototype.render = function(){
+ var game = new Game(title, type);
+};
+
+
+var GameLibrary = function(type){
+ //title of library (like "video games, etc")
+ this.type = type;
+ //array of all the Game instances that match the library title
+ this.games = [];
+};
+
+// GameLibrary.prototype.render = function(){
+
+// };
+
+
+Game.prototype.organize = function(libraries){
+ //var toUse;
+ for (var i = 0; i < libraries.length; i++) {
+   if(this.type === libraries[i].type){
+     libraries[i].games.push(this);
+     //libraries[i].games = toUse;
+   }
+ }
 };
 
 
 
+//GAME LIBRARY STUFF
+var videoGames = new GameLibrary("Video Games");
+var boardGames = new GameLibrary("Board Games");
+var drinkingGames = new GameLibrary("Drinking Games");
+var libraries = [videoGames, boardGames, drinkingGames];
+
+//INDIVIDUAL GAMES STUFF
+var marioCart = new Game("Mario Cart", "Video Games");
+// var mC = marioCart.render();
+
+//console.log(marioCart.organize(libraries));
+var temp = new Game();
+
 $(document).on('ready', function() {
-  console.log('sanity check!');
+
+ $('form').on('submit', function(event){
+   event.preventDefault();
+   var titleInput = $('#gameinput').val();
+   var gameType = $('#gametypes').val();
+
+   var game = new Game(titleInput, gameType);
+   temp = game;
+   temp.organize(libraries);
+   console.log(drinkingGames, boardGames, videoGames);
+
+   //console.log(temp);
+  $('.input').val("");
+
+ //$('body').append(titleInput + "  " + gType);
+  $('body').append("<p> Game Added --  Title:" +temp.title + " Type: " + temp.type + "</p>");
+
+
+
+ });
+
 });
